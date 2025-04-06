ee16ee248eb97ca01293d72a97c12b4501f407d5
Kevin <kevin@copperbay>
2025-04-06 09:03:03 -0500

    init

---
+++ server/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import testimonials from "./routes/testimonial.js";
+
+const PORT = process.env.PORT || 5050;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/testimonials", testimonials);
+
+// start the Express server
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
