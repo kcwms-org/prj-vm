f4e4aa035737b6d595b3e321f0150ca70a3c5834
kevin@metalwave <kevcoder@gmail.com>
2026-07-11 13:10:48 -0500

    removed prj-site

--- prj-site/astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import vue from '@astrojs/vue';
-
-export default defineConfig({
-  integrations: [tailwind(), vue()],
-  site: 'https://www.ssdisabilityassoc.com',
-});
